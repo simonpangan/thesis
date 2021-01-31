77738ae6e2235b2c3730ef9a2b8967463191cc31
Simon Pangan <simonjoseph.pangan@gmail.com>
2021-01-31 22:03:54 +0800

    first commit

---
+++ app/Models/Accounts.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+
+class Accounts extends Authenticatable
+{
+    //old User class
+    //customize 
+    use HasFactory, Notifiable;
+
+    protected $table = 'Accounts';
+    protected $primaryKey = 'AccountID';
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'Name',
+        'Email',
+        'Username',
+        'Password',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'Password',
+        'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+
+//because you change password field name
+    public function getAuthPassword()
+    {
+      return $this->Password;
+    }
+}
---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('Accounts', function (Blueprint $table) {
+            $table->id('AccountID');
+            $table->string('Name');
+            $table->string('Username')->unique();
+            $table->string('Email')->unique();
+            $table->timestamp('EmailVerifiedAt')->nullable();
+            $table->string('Password');
+            $table->RememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('Accounts');
+    }
+}
